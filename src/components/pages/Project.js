503a66fe53692e068735840ad4fab8ff6675f48b
Andrew <ATHur1104@gmail.com>
2023-08-23 01:18:27 -0500

    last commit of the night

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -1,38 +1,45 @@
 import './project.css';
 import React from 'react';
-import {SoggyGrogg} from '../images/';
+import { SoggyGrogg } from '../images/';
 
 const styles = {
   color: "white"
 }
+const handleSoggyGroggGithub = () => {
+  window.location.href = 'https://github.com/Project01Team4/soggy-grogg-and-the-wondrous-bog';
+};
+
+const handleSoggyGroggPlay = () => {
+  window.location.href = 'https://project01team4.github.io/soggy-grogg-and-the-wondrous-bog/';
+};
 
 export default function Project() {
+
   return (
 
-      <div className='container'>
+    <div className='container'>
       <h1>My Projects</h1>
-        <div className="card">
-          <h2>Soggy Grogg and the Wondrous Bog</h2>
-          <img src={SoggyGrogg} alt='Image of the title page of my first game' width="100%" height="100%" />
-          <h3>My first game!</h3>
-          <a href='https://github.com/Project01Team4/soggy-grogg-and-the-wondrous-bog'>
-            <p style={styles}>GitHub Repo</p>
-          </a>
-          <a href='https://project01team4.github.io/soggy-grogg-and-the-wondrous-bog/'>
-            <p style={styles}>Play The Game!</p>
-          </a>
+      <div className="card">
+        <h2>Soggy Grogg and the Wondrous Bog</h2>
+        <img src={SoggyGrogg} alt='Image of the title page of my first game' width="100%" height="100%" />
+        <h3>
+          My first game! In this game you play as an adventurer to fight randomly generated monsters that were populated using the DND 5e API. The game is made using HTML, CSS, and Javascript.
+        </h3>
+        <button style={styles} onClick={handleSoggyGroggGithub}>GitHub Repo</button>
+        <button style={styles} onClick={handleSoggyGroggPlay}>Play The Game!</button>
 
-        </div>
-        <div className="card">
-          This is the second Project
-        </div>
-        <div className="card">
-          This is the third Project
-        </div>
-        <div className="card">
-          This is the fourth Project
-        </div>
+
       </div>
+      <div className="card">
+        This is the second Project
+      </div>
+      <div className="card">
+        This is the third Project
+      </div>
+      <div className="card">
+        This is the fourth Project
+      </div>
+    </div>
 
   );
 }
