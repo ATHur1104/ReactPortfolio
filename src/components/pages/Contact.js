96c7e9503e4c10445a225d0582846459689c5455
Andrew <ATHur1104@gmail.com>
2023-08-24 20:11:23 -0500

    fixing minor styling issues

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -2,14 +2,14 @@ import React from 'react';
 import './Contact.css';
 
 export default function Contact() {
-const email = 'athur1104@gmail.com'
+const email = 'ATHur1104@gmail.com'
 const phoneNumber = '224-392-4660'
 
   return (
     <div className='container'>
       <h1>Contact</h1>
-      <a className='contact' href={`mailto:${email}`}>Email Me @ {email}</a>
-      <a className='contact'href={`tel:${phoneNumber}`}>Call me @ {phoneNumber}</a>
+      <a className='contact' href={`mailto:${email}`}>Click to Email Me @ {email}</a>
+      <a className='contact'href={`tel:${phoneNumber}`}>Click to Call me @ {phoneNumber}</a>
     </div>
   );
 }
